6bee424f6c4c3684b6c425fba9b22ecc62cf230e
RuoningJin <ronjin2013@hotmail.com>
2022-10-26 20:27:54 -0400

    created input code to refactor the play.js file.

---
+++ input.js
@@ -0,0 +1,20 @@
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput(handleUserInput);
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", (data) => {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  });
-
-  return stdin;
-};
 
 setupInput();
